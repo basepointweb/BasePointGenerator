4cbd3f045c3e379f64f10a70cdfb472bc5a34fe3
basepointweb <basepointweb@gmail.com>
2025-03-12 16:25:02 -0300

    Update template icons

--- source.extension.cs
+++ source.extension.cs
@@ -11,7 +11,7 @@ namespace BasePointGenerator
         public const string Name = "BasePoint generator";
         public const string Description = @"Generate clean architecture structure with BasePoint framework";
         public const string Language = "en-US";
-        public const string Version = "0.0.3";
+        public const string Version = "0.0.4";
         public const string Author = "Elias da Silva Rita";
         public const string Tags = "C#, CSharp,Clean architecture, DDD, OOP, Repository, Unit Tests, Dapper, Productivity, Scaffolding";
     }
